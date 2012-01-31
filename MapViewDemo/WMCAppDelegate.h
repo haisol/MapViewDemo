a0e24e794cee474d4844f687baa84f20c1d92f66
Kevin <kevin@Admins-MacBook.local>
2012-01-30 22:59:13 -0700

    Initial Commit

---
+++ MapViewDemo/WMCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WMCAppDelegate.h
+//  MapViewDemo
+//
+//  Created by Kevin on 1/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WMCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MapViewDemo/WMCViewController.h
@@ -0,0 +1,13 @@
+//
+//  WMCViewController.h
+//  MapViewDemo
+//
+//  Created by Kevin on 1/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WMCViewController : UIViewController
+
+@end
